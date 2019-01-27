26340d32ec0f04b136bc716bcabeec27361fc23f
Advait Junnarkar <advait.ju@gmail.com>
2019-01-27 21:19:34 +1100

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectId} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -30,6 +31,32 @@ app.get('/todos', (req, res) => {
     res.status(400).send(error)
   })
 })
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.status(200).send({todo})
+  }).catch((error) => res.status(404).send())
+})
+
+  // Validate ID
+    // Failure: 404, send empty send
+
+  // findById
+    // success
+      // if todo - send it back
+      // if no todo - 404, send empty send
+    // error
+      // 404 - send empty send
 
 app.listen(3000, () => {
   console.log('Started on port 3000')
